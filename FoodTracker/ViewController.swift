f247f6a51d1d757decd2417aa203cbd698e5b0dc
万一 <wanyi@wanyideMacBook-Pro.local>
2016-10-22 10:10:49 +0800

    Create outlets for text field and label in ViewController.swift

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
