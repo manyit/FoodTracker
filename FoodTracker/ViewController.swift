dad5c678446edb9d23f1e27c85ddbd3ae2724ac1
万一 <wanyi@wanyideMacBook-Pro.local>
2016-10-22 13:44:30 +0800

    ViewController.swift adopt UITextField protocol and set as the delegate for nameTextField, implement protocol method to resign first responder and pass the text field value to label

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
@@ -16,7 +16,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn (textField: UITextField) -> Bool {
+        //Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing (textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
 
     //MARK: Actions
